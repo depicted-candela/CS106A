cdb7662083aa9300338cfdb1c7fdc705648ec281
Depiction <nicalcoca@gmail.com>
2023-10-06 21:33:11 -0500

    Beginning of second graph of the PatchworkQuilt class

--- src/object_oriented_graphics/PQ.java
+++ src/object_oriented_graphics/PQ.java
@@ -3,10 +3,7 @@
  */
 package object_oriented_graphics;
 
-/**
- * 
- */
-
+import java.awt.Color;
 import acm.graphics.*;
 import acm.program.*;
 import objects_classes.RationalPrinceton;
@@ -21,20 +18,21 @@ public class PQ extends GraphicsProgram {
 	
 	private GCompound LogCabinBlock(RationalPrinceton sx, RationalPrinceton sy, RationalPrinceton s) {
 		
-		GCompound GC = new GCompound();
+		GCompound GC = new GCompound();		// to create the Compound
+											
+											// to initialize vars
 		RationalPrinceton width_x_h, height_y_h, width_x_v, height_y_v;
+											// to vary the positio
 		RationalPrinceton pos = new RationalPrinceton(0, 1);
-		
-		
-		
+											// to vary the position over the repetitive graph
 		width_x_h = sx.times(s.minus(new RationalPrinceton(1, 1)).divides(s));
 											// width for x base in horizontal bars (even)
 		height_y_h = sy.divides(s);
 											// height for y base in horizontal bars (even)
 		width_x_v = sx.divides(s);
-											// width for y base in 
+											// width for y base in vertical bars (odd)
 		height_y_v = sy.times(s.minus(new RationalPrinceton(1, 1)).divides(s));
-											// height for x base in
+											// height for x base in vertical bars (odd)
 		double odd = 90.0;
 		double even = 90.0;
 			
@@ -68,17 +66,61 @@ public class PQ extends GraphicsProgram {
 			
 		}
 		
+		for (int i = 0; i < GC.getElementCount(); i++) {
+			GRect gr = (GRect) GC.getElement(i);
+			gr.setFillColor(Color.GREEN);
+			gr.setFilled(true);
+		}
+		
+		return GC;
+		
+	}
+	
+	/**
+	 * @param args
+	 */
+	
+	private GCompound Squares(RationalPrinceton sx, RationalPrinceton sy) {
+		
+		GCompound GC = new GCompound();		// to create the Compound
+		RationalPrinceton width_x_h, height_y_h, width_x_v, height_y_v;
+											// to vary the position over the repetitive graph
+		RationalPrinceton posx = new RationalPrinceton(0, 1);
+											// x position of the square
+		RationalPrinceton posy = new RationalPrinceton(0, 1);
+											// y position of the square
+											
+		double angle = 45.0;				// to iterate with trigonometry to vary the inclination
+		
+//		GC.add(new GRect(sx.toDouble(), sy.toDouble()), posx.toDouble(), posy.toDouble());
+		GRect r1 = new GRect(sx.toDouble(), sy.toDouble());
+		GPolygon p1 = new GPolygon();
+		for (int i = 1; i < 4; i++) {
+			if (i != 0) {
+				p1.addPolarEdge(angle, sx.toDouble());
+			}
+		}
+		GC.add(r1, posx.toDouble(), posy.toDouble());
+		
 		return GC;
 		
 	}
 	
 	public void run() {
-		
+		// Define the amount of graphs per row in screen
 		RationalPrinceton SIZE_X = new RationalPrinceton(getWidth(), 5);
+		// Define the amount of graphs per column in screen
 		RationalPrinceton SIZE_Y = new RationalPrinceton(getHeight(), 3);
-		GCompound gc = LogCabinBlock(SIZE_X, SIZE_Y, new RationalPrinceton(9, 1));
-		add(gc);
 		
+		// Green graphs
+		GCompound gc1 = LogCabinBlock(SIZE_X, SIZE_Y, new RationalPrinceton(7, 1));
+		add(gc1, 0.0, 0.0);
+		GCompound gc2 = LogCabinBlock(SIZE_X, SIZE_Y, new RationalPrinceton(7, 1));
+		add(gc2, SIZE_X.times(new RationalPrinceton(1, 1)).toDouble(), SIZE_Y.times(new RationalPrinceton(2, 1)).toDouble());
+		
+		// Squares
+		GCompound s1 = Squares(SIZE_X, SIZE_Y);
+		add(s1, 0.0, SIZE_Y.times(new RationalPrinceton(1, 1)).toDouble());
 	}
 	
 	public static void main(String[] args) {
