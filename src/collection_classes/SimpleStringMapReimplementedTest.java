19a4982ac1eff4650270ebd690111eef09e9ae9d
Depiction <nicalcoca@gmail.com>
2023-11-06 13:31:51 -0500

    Chapter 13 started

---
+++ src/collection_classes/SimpleStringMapReimplementedTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package collection_classes;
+
+/**
+ * 
+ */
+public class SimpleStringMapReimplementedTest {
+
+	/**
+	 * @param args
+	 */
+	public void run() {
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new SimpleStringMapReimplementedTest().start();
+	}
+
+}
