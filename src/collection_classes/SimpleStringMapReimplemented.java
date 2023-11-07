ee6d691a01d0727eb19463a77dbc3d82a330e4a7
Depiction <nicalcoca@gmail.com>
2023-11-07 13:14:41 -0500

    First point of the Chapter 13

--- src/collection_classes/SimpleStringMapReimplemented.java
+++ src/collection_classes/SimpleStringMapReimplemented.java
@@ -1,10 +1,17 @@
 package collection_classes;
+import java.util.*;
 
 public class SimpleStringMapReimplemented {
 
 	/** Creates a new SimpleStringMap with no key/value pairs */
+	@SuppressWarnings("unchecked")
 	public SimpleStringMapReimplemented() {
-		bucketArray = new HashEntry[N_BUCKETS];
+		bucketArray = new ArrayList[2][INI_BUCKETS];
+		N_BUCKETS = INI_BUCKETS;
+		for (int i = 0; i < N_BUCKETS; i++) {
+			bucketArray[0][i] = new ArrayList<>();
+			bucketArray[1][i] = new ArrayList<>();
+		}
 	}
 	/**
 	* Sets the value associated with key. Any previous value for key is lost.
@@ -12,86 +19,67 @@ public class SimpleStringMapReimplemented {
 	* @param value The new value to be associated with key
 	*/
 	public void put(String key, String value) {
-		int bucket = Math.abs(key.hashCode()) % N_BUCKETS;
-		HashEntry entry = findEntry(bucketArray[bucket], key);
-		if (entry == null) {
-			entry = new HashEntry(key, value);
-			entry.setLink(bucketArray[bucket]);
-			bucketArray[bucket] = entry;
-		} else {
-			entry.setValue(value);
+		int bucket = Math.abs(key.hashCode());
+		if (bucket > N_BUCKETS) rearranging(bucket * 2);
+		bucketArray[0][bucket].add(key);
+		bucketArray[1][bucket].add(value);
+	}
+	
+	/*
+	* Scans the entry chain looking for an entry that matches the specified key.
+	* If no such entry exists, findEntry returns null.
+	*/
+	private String[] findEntry(ArrayList<String>[][] array, String key) {
+		String[] re = new String[2];
+		int bucket = Math.abs(key.hashCode());
+		if (array[0][bucket].contains(key)) {
+			int i = array[0][bucket].indexOf(key);
+			re[0] = array[0][bucket].get(i);
+			re[1] = array[1][bucket].get(i);
+			return re;
+		}
+		return null;
+	}
+	
+	/**
+	* Rearrange the array of values and keys to make possible to store more values given a new highest hashcode.
+	* @param bucket The new highest bucket to iterate
+	* @return The value associated with key, or null if no such value exists
+	*/
+	@SuppressWarnings("unchecked")
+	private void rearranging(int bucket) {
+		N_BUCKETS = bucket;
+		tempArray = bucketArray;
+		bucketArray = new ArrayList[2][bucket];
+		for (int i = 0; i < bucketArray[0].length; i++) {
+			if (i < tempArray.length) {
+				bucketArray[0][i] = tempArray[0][i];
+				bucketArray[1][i] = tempArray[1][i];
+			} else {
+				bucketArray[0][i] = new ArrayList<>();
+				bucketArray[1][i] = new ArrayList<>();
+			}
 		}
 	}
+	
 	/**
 	* Retrieves the value associated with key, or null if no such value exists.
 	* @param key The key used to look up the value
 	* @return The value associated with key, or null if no such value exists
 	*/
 	public String get(String key) {
-		int bucket = Math.abs(key.hashCode()) % N_BUCKETS;
-		HashEntry entry = findEntry(bucketArray[bucket], key);
+		String[] entry = findEntry(bucketArray, key);
 		if (entry == null) {
 			return null;
 		} else {
-			return entry.getValue();
+			return entry[1];
 		}
-	}
-	/*
-	* Scans the entry chain looking for an entry that matches the specified key.
-	* If no such entry exists, findEntry returns null.
-	*/
-	private HashEntry findEntry(HashEntry entry, String key) {
-		while (entry != null) {
-		if (entry.getKey().equals(key)) return entry;
-			entry = entry.getLink();
-		}
-			return null;
 	}
 	
 	/* Private constants */
-	private static final int N_BUCKETS = 7;
+	private static final int INI_BUCKETS = 7;
+	public int N_BUCKETS;
 	/* Private instance variables */
-	private HashEntry[] bucketArray;
-}
-
-/* Package-private class: HashEntry */
-/*
-* This class represents a pair of a key and a value, along with a reference
-* to the next HashEntry in the chain. The methods exported by the class
-* consist only of getters and setters.
-*/
-class HashEntry {
-	/* Creates a new HashEntry for the specified key/value pair */
-	public HashEntry(String key, String value) {
-		entryKey = key;
-		entryValue = value;
-	}
-	/* Returns the key component of a HashEntry */
-	public String getKey() {
-		return entryKey;
-	}
-	/* Returns the value component of a HashEntry */
-	public String getValue() {
-		return entryValue;
-	}
-	/* Sets the value component of a HashEntry to a new value */
-	public void setValue(String value) {
-		entryValue = value;
-	}
-	/* Returns the next link in the entry chain */
-	public HashEntry getLink() {
-		return entryLink;
-	}
-	/* Sets the link to the next entry in the chain */
-	public void setLink(HashEntry nextEntry) {
-		entryLink = nextEntry;
-	}
-	/* Private instance variables */
-	private String entryKey;
-	/* The key component for this HashEntry
-	*/
-	private String entryValue;
-	/* The value component for this HashEntry
-	*/
-	private HashEntry entryLink; /* A reference to the next entry in the chain */
+	public ArrayList<String>[][] bucketArray;
+	public ArrayList<String>[][] tempArray;
 }
