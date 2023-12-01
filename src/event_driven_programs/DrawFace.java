34338c6ed78c101fbf819461bcf13f464fbde9d8
DEPICTION <nicalcoca@gmail.com>
2023-12-01 14:44:41 -0500

    Point 3 of Chapter 10 until finished

--- src/event_driven_programs/DrawFace.java
+++ src/event_driven_programs/DrawFace.java
@@ -3,8 +3,9 @@
  */
 package event_driven_programs;
 
-import acm.graphics.*;
+import java.awt.event.*;
 import acm.program.*;
+import acm.graphics.*;
 import object_oriented_graphics.Face;
 
 /**
@@ -12,18 +13,44 @@ import object_oriented_graphics.Face;
  */
 public class DrawFace extends GraphicsProgram {
 
+	private static final long serialVersionUID = 6631039572178869164L;
+
 	/**
 	 * 
 	 */
 
-	public void run() {
-		Face f = new Face(SIZE);
-		add(f.head, getWidth(), getHeight());
+	public void init() {
+		F = new Face(SIZE);
+		POS_X = getWidth() / 2 - ((5 * SIZE) / 12);
+		POS_Y = getHeight() / 2 - SIZE / 2;
+		add(F, POS_X, POS_Y);
+		addMouseListeners();
 	}
+	
+	public void mousePressed(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+		GOval eye = (GOval) F.getElement(2);
+		F.see(x, y, eye.getX() + POS_X, eye.getY() + POS_Y);
+	}
+	
+	public void mouseDragged(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+		GOval eye = (GOval) F.getElement(2);
+		F.see(x, y, eye.getX() + POS_X, eye.getY() + POS_Y);
+	}
+	
+	public void mouseReleased(MouseEvent e) {
+		F.defaultPupilsPosition();
+	}
+	
 	public static void main(String[] args) {
 		new DrawFace().start();
 	}
 	
-	private int SIZE = 100;
-
+	private double POS_X, POS_Y;
+	private int SIZE = 300;
+	private Face F;
+	
 }
--- src/object_oriented_graphics/Face.java
+++ src/object_oriented_graphics/Face.java
@@ -6,18 +6,73 @@ import acm.graphics.*;
 /**
  * 
  */
-public class Face {
-
+public class Face extends GCompound {
+	private static final long serialVersionUID = -1073946827061972497L;
 	/**
 	 * 
 	 */
+	private GOval PUPIL_L;
+	private double SIZE, PUPIL_L_X, PUPIL_L_Y;
+	private double SCALE, SCALE_EYE_X, SCALE_EYE_Y, SCALE_PUPIL, EYE_L_X, EYE_L_Y, RADIO;
+	private double SCALE_X_N 	= 4;
+	private double SCALE_X_D 	= 6;
+	private double EYE			= 8;
+	
 	public Face(double size) {
 		
-		head.add(new GOval(size, 5 * size / 6));
-		eye_l.add(new GOval(size / 8, (5 * size / 6) / 8));
-		head.add(eye_l, size / 8 * 2, ((5 * size / 6) / 8) * 2);
+		headShape(size);
+		eyesPosition();
+		add(new GOval(SCALE, SIZE));
+		add(new GOval(SCALE_EYE_X, SCALE_EYE_Y), EYE_L_X, EYE_L_Y);
+		
+		pupilsPosition();
+		PUPIL_L 	= new GOval(SCALE_PUPIL, SCALE_PUPIL);
+		PUPIL_L.setFilled(true);
+		add(PUPIL_L, PUPIL_L_X, PUPIL_L_Y);
+		
+		RADIO = SCALE_EYE_X / 2 - SCALE_PUPIL / 2;
 		
 	}
-	public GCompound head, eyes, eye_l, eye_r;
+	
+	private void headShape(double size) {
+		SIZE 		= size;
+		SCALE 		= SCALE_X_N * SIZE / SCALE_X_D;
+		SCALE_EYE_X = SCALE_X_N * SIZE / (SCALE_X_D * EYE);
+		SCALE_EYE_Y = SIZE / EYE;
+	}
+	
+	public void eyesPosition() {
+		EYE_L_X = 1 * SCALE / 6;
+		EYE_L_Y = 2 * SCALE / 5;
+	}
+	
+	public void pupilsPosition() {
+		SCALE_PUPIL	= SIZE / 30;
+		PUPIL_L_X 	= EYE_L_X + SCALE_EYE_X / 2 - SCALE_PUPIL / 2;
+		PUPIL_L_Y 	= EYE_L_Y + SCALE_EYE_Y / 2 - SCALE_PUPIL / 2;
+	}
+	
+	public void defaultPupilsPosition() {
+		getElement(2).setLocation(PUPIL_L_X, PUPIL_L_Y);;
+	}
+	
+	public void see(double xm, double ym, double ex, double ey) {
+		double Delta_x 	= xm - ex;
+		double Delta_y 	= ym - ey;
+		double theta	= Math.atan(Delta_y / Delta_x);
+		if (Delta_x > 0) {
+			double delta_x	= RADIO * Math.cos(theta);
+			double delta_y	= RADIO * Math.sin(theta);
+			PUPIL_L.setLocation(
+					PUPIL_L_X + delta_x,
+					PUPIL_L_Y + delta_y);	
+		} else {
+			double delta_x	= RADIO * Math.cos(theta);
+			double delta_y	= RADIO * Math.sin(theta);
+			PUPIL_L.setLocation(
+					PUPIL_L_X - delta_x,
+					PUPIL_L_Y - delta_y);	
+		}
+	}
 	
 }
